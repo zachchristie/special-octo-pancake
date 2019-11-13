15afbb8b85874be063d623e8015b41343185c256
Zach Christie <zachchristieofficial@gmail.com>
2019-11-13 12:34:16 -0700

    first

---
+++ graphql-api/src/entity/Points.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './User';
+
+@Entity()
+export class Points {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+  date: Date;
+
+  @Column()
+  exercise: number;
+
+  @Column()
+  diet: number;
+
+  @Column()
+  notes: string;
+
+  @ManyToOne(() => User, user => user.points, { cascade: ["insert"] })
+  user: User|null;
+}
